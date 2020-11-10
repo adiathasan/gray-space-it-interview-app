519224b4a965a1d5c9518b7737b333f0d68002d3
adiathasan <adiathasan.me@gmail.com>
2020-11-09 19:10:07 -0800

    blog and blog details page with pagination and post limit done

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 import requests
 
 url = 'https://jsonplaceholder.typicode.com'
@@ -7,10 +7,16 @@ url = 'https://jsonplaceholder.typicode.com'
 
 
 def posts(request):
-    res = requests.get(f'{url}/posts')
-    posts = res.json()
-    context = {"posts": posts}
-    return render(request, 'blog-listing.html', context)
+    start_post = request.GET.get("start", 0)
+    limit_post = request.GET.get("limit", 10)
+    res = requests.get(f'{url}/posts?_start={start_post}&_limit={limit_post}')
+    if res:
+        posts = res.json()
+        context = {"posts": posts, "pagination":  range(
+            1, int(100/int(limit_post))),  "limit_post": int(limit_post), "active": int(start_post)/int(limit_post), "title": "CodeBlog"}
+        return render(request, 'blog-listing.html', context)
+
+    return HttpResponse("Oops! please check your network connection")
 
 
 # POST DETAILS VIEW ENDPOINT
@@ -29,5 +35,6 @@ def post_details(request, pk):
     res_user = requests.get(f'{url}/users/{dict(post).get("userId")}')
     user = res_user.json()
 
-    context = {"post": post, "comments": comments, "user": user}
+    context = {"post": post, "comments": comments,
+               "user": user, "title": f"CodeBlog | {dict(post).get('title')}"}
     return render(request, 'blog-post.html', context)
