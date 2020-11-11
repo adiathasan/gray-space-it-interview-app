09e2b49edb107f74a8d44a170cb21bb3f687d8a6
adiathasan <adiathasan.me@gmail.com>
2020-11-11 11:15:21 -0800

    server running on heroku

--- client/src/config/Axios.js
+++ client/src/config/Axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const instanceOfAxios = axios.create({
-	baseURL: 'http://127.0.0.1:8000/api',
+	// baseURL: 'http://127.0.0.1:8000/api',
+	baseURL: 'https://codeblog-react-django.herokuapp.com/api',
 });
 
 export const instanceOfAxiosPosts = axios.create({
